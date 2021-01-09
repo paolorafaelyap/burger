aa99de0690dfdd412061ff19fa9c3441fe5f59bf
paolorafaelyap <paolorafaelyap@gmail.com>
2021-01-09 10:17:33 -0800

    added table for burgers and included fields

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,17 @@
 DROP DATABASE IF EXISTS burgers_db;
--- Creates the "animals_db" database --
+-- Creates the "burgers_db" database --
 CREATE DATABASE burgers_db;
 
--- Makes it so all of the following code will affect animals_db --
+-- Makes it so all of the following code will affect burgers_db --
 USE burgers_db;
+
+CREATE TABLE burgers (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    -- creates string column that is not null
+    burger_name VARCHAR(100) NOT NULL,
+    -- creates boolean column where default value is false if nothing is entered
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
+
+
+);
