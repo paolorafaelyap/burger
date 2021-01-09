10827c6895da34c93a954676252b745266de0a66
paolorafaelyap <paolorafaelyap@gmail.com>
2021-01-09 10:09:36 -0800

    created database burgers_db

---
+++ db/schema.sql
@@ -0,0 +1,6 @@
+DROP DATABASE IF EXISTS burgers_db;
+-- Creates the "animals_db" database --
+CREATE DATABASE burgers_db;
+
+-- Makes it so all of the following code will affect animals_db --
+USE burgers_db;
