e8e3f248dd0b40c8d6ec6009a2ea25ba379fd876
paolorafaelyap <paolorafaelyap@gmail.com>
2021-01-09 20:14:19 -0800

    added config and updated seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bacon Cheeseburger", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Jalapeno Burger", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Steak Burger", false);
