0187a8b27ac8d3b2839020f3717522de3f99813c
paolorafaelyap <paolorafaelyap@gmail.com>
2021-03-12 20:38:32 -0800

    updated server.js

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
 const express = require("express");
+
+var PORT = process.env.PORT || 8000;
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var expressHandlebars = require("express-handlebars");
+
+app.engine("handlebars", expressHandlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("Listening on port:%s", PORT);
+});
