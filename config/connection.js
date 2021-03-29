f7d4ad734a1c9a42fb695d0b31eda00e36d588c0
paolorafaelyap <paolorafaelyap@gmail.com>
2021-03-29 12:30:07 -0700

    updated server connection

--- config/connection.js
+++ config/connection.js
@@ -3,9 +3,9 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   host: "localhost",
-  port: 8080,
+  port: 3306,
   user: "root",
-  password: "",
+  password: "Balls8927!",
   database: "burgers_db"
 });
 
